e26c3b0efca9c8d15d08fcc7b6fd18865db24607
Max Rogers <max@carrotcreative.com>
2015-06-08 11:00:57 -0400

    init

---
+++ iOSTravisTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTravisTemplate
+//
+//  Created by Max Rogers on 6/8/15.
+//  Copyright (c) 2015 Max Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSTravisTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSTravisTemplate
+//
+//  Created by Max Rogers on 6/8/15.
+//  Copyright (c) 2015 Max Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
